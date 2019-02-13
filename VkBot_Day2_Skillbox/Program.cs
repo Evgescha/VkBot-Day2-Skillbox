903b60ec3a3d6445bd268c39499e85db2e6b5e43
Evgescha <EvgeschaMaslov007@yandex.ru>
2019-02-13 12:25:31 +0300

    Добавьте файлы проекта.

---
+++ VkBot_Day2_Skillbox/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VkBot_Day2_Skillbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
